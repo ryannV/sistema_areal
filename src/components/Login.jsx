a65ccee496a384e189a65766b34107df4a1c32d6
Ryann Victor <ryannvictor72@gmail.com>
2025-03-09 15:16:03 -0300

    vai por favor

---
+++ src/components/Login.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import styles from "./Login.module.css";
+import logo from "../assets/areal-logo-sf.png";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.container_second}>
+        <img src={logo} alt="logo" />
+
+        <div className={styles.campos}>
+          <h3>Icon</h3>
+          <input type="text" placeholder="Digite seu Usuário" />
+        </div>
+
+        <div className={styles.campos}>
+          <h3>Icon</h3>
+          <input type="password" placeholder="Digite sua Senha" />
+        </div>
+
+        <div className={styles.submit}>
+          <button onClick={() => navigate("/main")}>Acessar</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
